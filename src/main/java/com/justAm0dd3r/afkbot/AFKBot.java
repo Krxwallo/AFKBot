1b899d07373e0698c70a8397a038b99895e807ed
Krxwallo <krxwallo@users.noreply.github.com>
2021-01-08 19:10:05 +0100

    Version 1.1.0

--- src/main/java/com/justAm0dd3r/afkbot/AFKBot.java
+++ src/main/java/com/justAm0dd3r/afkbot/AFKBot.java
@@ -16,7 +16,7 @@ public class AFKBot
 {
     public static final String MODID = "afkbot";
     public static final String NAME = "AFKBot";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.1.0";
 
     public static Logger logger;
 
